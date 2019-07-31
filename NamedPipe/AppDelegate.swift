65790ff37f7e1993da0a9a0a762078fbfeb6c2e3
Yuki Okubo <y-okubo@nekojarashi.com>
2019-07-31 15:45:34 +0900

    Initial Commit

---
+++ NamedPipe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NamedPipe
+//
+//  Created by Yuki Okubo on 7/31/19.
+//  Copyright © 2019 Nekojarashi Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
